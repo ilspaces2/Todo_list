8e969e23c1cabd96ec05e351d8278338dd9bce56
I_M <moskva.88@mail.ru>
2022-06-27 13:39:34 +0300

    Add interface Store

--- src/main/java/ru/job4j/todo/persistence/ItemStore.java
+++ src/main/java/ru/job4j/todo/persistence/ItemStore.java
@@ -1,19 +1,16 @@
 package ru.job4j.todo.persistence;
 
 import net.jcip.annotations.ThreadSafe;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Item;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.function.Function;
 
 @ThreadSafe
 @Repository
-public class ItemStore {
+public class ItemStore implements Store {
 
     private final SessionFactory sessionFactory;
 
@@ -25,13 +22,13 @@ public class ItemStore {
         return tx(session -> {
             session.save(item);
             return item;
-        });
+        }, sessionFactory);
     }
 
     public boolean deleteById(final int id) {
         return tx(session -> session.createQuery((" delete from Item where id=:ID"))
                 .setParameter("ID", id)
-                .executeUpdate() > 0);
+                .executeUpdate() > 0, sessionFactory);
     }
 
     public boolean updateById(final int id, final Item item) {
@@ -45,7 +42,7 @@ public class ItemStore {
                 .setParameter("nDone", item.isDone())
                 .setParameter("nUserId", item.getUserId())
                 .setParameter("ID", id)
-                .executeUpdate() > 0);
+                .executeUpdate() > 0, sessionFactory);
     }
 
     public boolean updateByIdWhenDone(final int id) {
@@ -53,38 +50,23 @@ public class ItemStore {
                 .setParameter("nFinished", LocalDateTime.now())
                 .setParameter("nDone", true)
                 .setParameter("ID", id)
-                .executeUpdate() > 0);
+                .executeUpdate() > 0, sessionFactory);
     }
 
     public Item findById(final int id) {
         return (Item) tx(session -> session.createQuery("from Item where id=:ID")
                 .setParameter("ID", id)
-                .uniqueResult());
+                .uniqueResult(), sessionFactory);
     }
 
     public List<Item> findAllByIdUser(final int idUser) {
         return tx(session -> session.createQuery("from Item where userId=:nUserID ")
                 .setParameter("nUserID", idUser)
-                .list());
+                .list(), sessionFactory);
     }
 
     public boolean deleteAll() {
         return tx(session -> session.createQuery(" delete from Item ")
-                .executeUpdate() > 0);
-    }
-
-    private <T> T tx(final Function<Session, T> command) {
-        final Session session = sessionFactory.openSession();
-        final Transaction tx = session.beginTransaction();
-        try {
-            T rsl = command.apply(session);
-            tx.commit();
-            return rsl;
-        } catch (final Exception e) {
-            session.getTransaction().rollback();
-            throw e;
-        } finally {
-            session.close();
-        }
+                .executeUpdate() > 0, sessionFactory);
     }
 }
--- src/main/java/ru/job4j/todo/persistence/UserStore.java
+++ src/main/java/ru/job4j/todo/persistence/UserStore.java
@@ -1,18 +1,15 @@
 package ru.job4j.todo.persistence;
 
 import net.jcip.annotations.ThreadSafe;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.User;
 
 import java.util.Optional;
-import java.util.function.Function;
 
 @ThreadSafe
 @Repository
-public class UserStore {
+public class UserStore implements Store {
     private final SessionFactory sessionFactory;
 
     public UserStore(SessionFactory sessionFactory) {
@@ -24,7 +21,7 @@ public class UserStore {
             return tx(session -> {
                 session.save(user);
                 return Optional.of(user);
-            });
+            }, sessionFactory);
         } catch (Exception e) {
             return Optional.empty();
         }
@@ -33,33 +30,18 @@ public class UserStore {
     public Optional<User> findById(int id) {
         return tx(session -> session.createQuery("from User where id=:ID")
                 .setParameter("ID", id)
-                .uniqueResultOptional());
+                .uniqueResultOptional(), sessionFactory);
     }
 
     public boolean deleteAll() {
         return tx(session -> session.createQuery("delete from User")
-                .executeUpdate() > 0);
+                .executeUpdate() > 0, sessionFactory);
     }
 
     public Optional<User> findUserByNameAndPass(String username, String pass) {
         return tx(session -> session.createQuery("from User where userName=:nName and pass=:nPass")
                 .setParameter("nPass", pass)
                 .setParameter("nName", username)
-                .uniqueResultOptional());
-    }
-
-    private <T> T tx(final Function<Session, T> command) {
-        final Session session = sessionFactory.openSession();
-        final Transaction tx = session.beginTransaction();
-        try {
-            T rsl = command.apply(session);
-            tx.commit();
-            return rsl;
-        } catch (final Exception e) {
-            session.getTransaction().rollback();
-            throw e;
-        } finally {
-            session.close();
-        }
+                .uniqueResultOptional(), sessionFactory);
     }
 }
